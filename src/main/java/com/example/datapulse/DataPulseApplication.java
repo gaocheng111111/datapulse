b98bec2ef5a55f6f40dcc87dac9ae11404a157ae
gaocheng111111 <gaocheng111111@126.com>
2024-03-31 17:43:54 +0800

    update

--- src/main/java/com/example/datapulse/DataPulseApplication.java
+++ src/main/java/com/example/datapulse/DataPulseApplication.java
@@ -1,13 +1,7 @@
 package com.example.datapulse;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DataPulseApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(DataPulseApplication.class, args);
+        // Your application logic goes here
     }
-
 }
---
+++ src/main/java/com/sqlywgl/project/model/Database.java
@@ -0,0 +1,32 @@
+package com.sqlywgl.project.model;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "javasj_database")
+public class Database {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "database_id")
+    private String databaseId;
+    @Column(name = "database_name")
+    private String databaseName;
+    @Column(name = "database_type")
+    private String databaseType;
+    @Column(name = "database_version")
+    private String databaseVersion;
+    @Column(name = "database_size")
+    private String databaseSize;
+    @Column(name = "database_status")
+    private boolean databaseStatus;
+    @Column(name = "create_user")
+    private int createUser;
+    @Column(name = "update_user")
+    private int updateUser;
+    @Column(name = "create_time")
+    private int createTime;
+    @Column(name = "update_time")
+    private int updateTime;
+    @Column(name = "mark")
+    private boolean mark;
+}
